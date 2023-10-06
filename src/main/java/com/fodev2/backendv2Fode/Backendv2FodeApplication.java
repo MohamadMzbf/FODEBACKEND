807d59e6538398a00e97dbbe150da9558f6110d5
Saydina Sylla <saydina.sylla@unchk.edu.sn>
2023-10-06 12:26:25 +0000

    Initial Commit

---
+++ src/main/java/com/fodev2/backendv2Fode/Backendv2FodeApplication.java
@@ -0,0 +1,13 @@
+package com.fodev2.backendv2Fode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendv2FodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendv2FodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fodev2/backendv2Fode/Backendv2FodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fodev2.backendv2Fode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendv2FodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
