c54e71d0e349fef305e526dc0620facfb9692bb4
Mohamed <mohamadou.faye@unchk.edu.sn>
2023-10-31 15:13:22 +0000

    Commit befor pull

--- src/main/java/com/fodev2/backendv2Fode/Backendv2FodeApplication.java
+++ src/main/java/com/fodev2/backendv2Fode/Backendv2FodeApplication.java
@@ -2,6 +2,7 @@ package com.fodev2.backendv2Fode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
